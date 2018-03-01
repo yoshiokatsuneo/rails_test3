38b55f255cd2cee36774e37fe404e87e4dedca5b
Yoshioka Tsuneo <yoshiokatsuneo@gmail.com>
2018-03-01 10:16:50 +0900

    post2

---
+++ test/system/post2s_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class Post2sTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit post2s_url
+  #
+  #   assert_selector "h1", text: "Post2"
+  # end
+end
